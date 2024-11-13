59abb210202b7024c00a197a17ac8debea326cc5
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-11-13 15:19:54 +0300

    initial project setup

---
+++ backend/auth_app/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        ]
+        read_only_fields = ["is_active", "is_staff", "is_superuser"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "name",
+            "password",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if user:
+                if user.is_active:
+                    data["user"] = user
+                else:
+                    msg = "User is deactivated."
+                    raise serializers.ValidationError(msg)
+            else:
+                msg = "Unable to login with given credentials."
+                raise serializers.ValidationError(msg)
+        else:
+            msg = "Must provide email and password."
+            raise serializers.ValidationError(msg)
+        return data
